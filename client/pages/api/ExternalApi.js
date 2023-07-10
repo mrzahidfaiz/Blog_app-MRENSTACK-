973ec475ed2b07b9731bb5f5151fff3267620152
Muhammad Zahid faiz <mrzahidfaiz@gmail.com>
2023-07-10 16:22:07 +0500

    Update ExternalApi.js

--- client/pages/api/ExternalApi.js
+++ client/pages/api/ExternalApi.js
@@ -1,13 +1,15 @@
 import axios from 'axios';
 
-const API_KEY = process.env.API_KEY
+// const API_KEY = process.env.API_KEY
 // console.log(API_KEY)
-const NEWS_API_KEY = `https://newsapi.org/v2/everything?q=technology AND business&sortBy=publishedAt&language=en&apiKey=${API_KEY}`;
+// const NEWS_API_KEY = `https://newsapi.org/v2/everything?q=technology AND business&sortBy=publishedAt&language=en&apiKey=${API_KEY}`;
+
+const NEW_ADDRESS = 'https://saurav.tech/NewsAPI/top-headlines/category/business/us.json'
 
 export const GET_NEWS_API = async () => {
     let response;
     try {
-        response = await axios.get(NEWS_API_KEY)
+        response = await axios.get(NEW_ADDRESS)
         response = response.data.articles.splice(0, 54)
     } catch (error) {
         return error;
